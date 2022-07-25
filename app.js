2a53627c618453dba65629d9ac261619371ea239
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-25 17:06:29 -0300

    curso de JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var name = "Gabriel Fernandes"
+var age = 35
+var pernon = ['alan', 'gabriel', 'kaio']
+
+var person = {
+    name: "Gabriel Fernandes",
+    idade: age,
+    statusBancario: false,
+    rico: true
+}
+
+
+console.log(person)
