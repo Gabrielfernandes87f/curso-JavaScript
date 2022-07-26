64c94e23af13c3d84a31c714943d0cfaf77954cf
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-25 21:10:22 -0300

    function ? = if  : = else

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-// AND, OR, NOT
+var age = 18
+var result = age >= 18 ? 'maior de idade' : 'menor de idade'
 
-// AND = &&, OR = ||, NOT = !
-
-var sexo = 'f'
-
-if (!sexo === 'M') {
-    console.log('sexo Masculino')
-
-} else if (sexo === 'F' || 'f'){
-    console.log('sexo feminino')
-
-} else {
-    console.log('valor inválido')
-}
-
-// a sinal ! exclamação inverte o valor false vira true e vice versa
-
-var isActive = true
-isActive = !isActive
-
-console.log(isActive)
+console.log(result)
