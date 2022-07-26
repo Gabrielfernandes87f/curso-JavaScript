2a35b23dcc7b88ba84834409462cc3478d5a076e
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-26 04:16:47 -0300

    Metódos, trim, split, replace etc

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-// for (contagem; condição; incremento)
+var text = 'hello world';
 
-for (var count = 1; count <= 10; count++) {
-    console.log(count)
-}
-// i = interação
+var value = text.replace(' ', ' G ');
+console.log(value);
 
-for (i = 1; i <= 10; i++) {
-    console.log(i)
-}
-// up
+// resultado: hello G world
+// o que aconteceu: o metodo replace substituiu o primeiro caracter ' ' por ' G '
+
+// .lenght - retorna o tamanho da string
+// console.log(text.length);
+// o .trim - remove os espaços em branco no inicio e no fim da string
+// console.log(text.trim());
+// o .toUpperCase - converte a string para maiusculo
+// console.log(text.toUpperCase());
+// o .toLowerCase - converte a string para minusculo
+// console.log(text.toLowerCase());
+// o .charAt - retorna o caracter na posição especificada
+// console.log(text.charAt(0));
+// o .indexOf - retorna a posição do caracter na string
+// console.log(text.indexOf('o'));
+// o .substring - retorna uma parte da string
+// console.log(text.substring(0, 3));
+// o .slice - retorna uma parte da string
+// console.log(text.slice(0, 3));
+// o .split - divide a string em um array
+// console.log(text.split(' '));  // divide no espaço
+// o .replace - substitui um caracter por outro
+// console.log(text.replace(' ', ' G '));
+
+var site = 'www.google.com';
+var replacee = site.split('www.');
+console.log(replacee);
+
+// esse método não altera a string original
