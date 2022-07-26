3cdd6f782928d3a0e8445b530d518918aaa652a3
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-26 13:13:30 -0300

    add tag, class com variaveis usando addeventlist

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-var text = 'hello world';
+var button = document.querySelector('#body button');
+var card = document.querySelector('#body #card');
+var input = document.querySelector('#body input');
 
-var value = text.replace(' ', ' G ');
-console.log(value);
+button.addEventListener('click', function () {
+    card.innerHTML =  `Sua idade é: ` + input.value;
+});
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-// resultado: hello G world
-// o que aconteceu: o metodo replace substituiu o primeiro caracter ' ' por ' G '
+button.addEventListener('click', function () {
+    alert(`Seu email  é: ` + input.value)
+});
 
-// .lenght - retorna o tamanho da string
-// console.log(text.length);
-// o .trim - remove os espaços em branco no inicio e no fim da string
-// console.log(text.trim());
-// o .toUpperCase - converte a string para maiusculo
-// console.log(text.toUpperCase());
-// o .toLowerCase - converte a string para minusculo
-// console.log(text.toLowerCase());
-// o .charAt - retorna o caracter na posição especificada
-// console.log(text.charAt(0));
-// o .indexOf - retorna a posição do caracter na string
-// console.log(text.indexOf('o'));
-// o .substring - retorna uma parte da string
-// console.log(text.substring(0, 3));
-// o .slice - retorna uma parte da string
-// console.log(text.slice(0, 3));
-// o .split - divide a string em um array
-// console.log(text.split(' '));  // divide no espaço
-// o .replace - substitui um caracter por outro
-// console.log(text.replace(' ', ' G '));
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-var site = 'www.google.com';
-var replacee = site.split('www.');
-console.log(replacee);
+function mostrarEmail() {
+    card.innerHTML =  `Sua idade é (mostrarEmail): ` + input.value;
+}
 
-// esse método não altera a string original
+button.addEventListener('click', mostrarEmail);
+
+button.addEventListener('click', function () {
+    card.innerHTML = `
+    <div class="new">
+        <h1 class="title-admin">Novo acesso com admin,</h1>
+        <p>Seu email é: ` + input.value + `</p>
+    </div>
+</div>`
+});
