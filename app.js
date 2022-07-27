02273dda5660885b5d954e7ea1f205d158271e6d
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-27 17:38:03 -0300

    function possso passar um paramentro, esse pode ser um event(qualquer nome) nem recebo um obj com informaçoes do evento. o preventListener ligado nao vai carregar sua pagina auto

--- app.js
+++ app.js
@@ -2,28 +2,10 @@ var button = document.querySelector('#body button');
 var card = document.querySelector('#body #card');
 var input = document.querySelector('#body input');
 
-button.addEventListener('click', function () {
-    card.innerHTML =  `Sua idade é: ` + input.value;
+button.addEventListener('click', function (event) {
+    event.preventDefault();
+    card.innerHTML = `Sua idade é: ` + input.value;
+    console.log(input.value);
 });
+// 
 //////////////////////////////////////////////////////////////////////////////////////////////
-
-button.addEventListener('click', function () {
-    alert(`Seu email  é: ` + input.value)
-});
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-function mostrarEmail() {
-    card.innerHTML =  `Sua idade é (mostrarEmail): ` + input.value;
-}
-
-button.addEventListener('click', mostrarEmail);
-
-button.addEventListener('click', function () {
-    card.innerHTML = `
-    <div class="new">
-        <h1 class="title-admin">Novo acesso com admin,</h1>
-        <p>Seu email é: ` + input.value + `</p>
-    </div>
-</div>`
-});
