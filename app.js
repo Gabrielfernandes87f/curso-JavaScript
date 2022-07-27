680e27ba1c1de7fd0143790bcb35bb25209e3c24
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-27 17:49:33 -0300

    update preventDefault, event

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ button.addEventListener('click', function (event) {
     console.log(input.value);
 
 });
+
+//// uma function recebe um parametro e retorna um valor
+// posso colocar o parametro dentro de parenteses ee
+// retornar um obj com informações do evento.
+// exemplo function click(event) {...}
+// event.target.innerHTML = 'clicou';
+// esse nome event é um objeto que contem todas as informações do evento, como o target, o elemento que foi clicado, etc.
+// event é uma variavel, poderia ser qualquer nome que iria representar o evento. event apenas pra ficar mais facil de ler.
