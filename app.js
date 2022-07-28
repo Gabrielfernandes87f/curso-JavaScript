530975b4b1989dad42b6426215d10f53b1885895
Gabrielfernandes87f <gabrielfernandesfotografias@outlook.com>
2022-07-28 02:02:50 -0300

    usando conceito de promise com exemple de theme

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var button = document.querySelector('#body button');
-var card = document.querySelector('#body #card');
-var input = document.querySelector('#body input');
+localStorage.theme = 'light';
+var theme = localStorage.theme;
 
-button.addEventListener('click', function (event) {
-    event.preventDefault();
-    card.innerHTML = `Sua idade é: ` + input.value;
-    console.log(input.value);
 
-});
 
-//// uma function recebe um parametro e retorna um valor
-// posso colocar o parametro dentro de parenteses ee
-// retornar um obj com informações do evento.
-// exemplo function click(event) {...}
-// event.target.innerHTML = 'clicou';
-// esse nome event é um objeto que contem todas as informações do evento, como o target, o elemento que foi clicado, etc.
-// event é uma variavel, poderia ser qualquer nome que iria representar o evento. event apenas pra ficar mais facil de ler.
+var themePromise = new Promise(function (resolve, reject) {
+
+    var theme = localStorage.theme
+
+    if (theme == 'dark') {
+        resolve('dark')
+        console.log('dark')
+    } else {
+        reject('light')
+        console.log('light')
+    }
+})
+
+console.log(themePromise)
